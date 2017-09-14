f6330435bafcbe2d84723497ffecce41c8fd668b
user <user@home.com>
2017-09-14 19:10:31 +0800

    Add test cases for DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -57,7 +57,7 @@ app.delete('/todos/:id', (req, res) => {
     if (!todo) {
       res.status(404).send('todo not found!');
     };
-    res.send(todo);
+    res.send({todo});
   }).catch ((e) => {
     res.status(400).send();
   });
