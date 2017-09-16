eba30f415f94e07076aa37f0ac89250cba7a46e1
user <user@home.com>
2017-09-16 09:47:00 +0800

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -64,13 +64,29 @@ UserSchema.statics.findByToken = function (token) {
     // })
     return Promise.reject();
   }
+};
 
-  return User.findOne({
-    '_id': decoded._id,   //quote added for consistency. Not needed
-    'tokens.token': token,
-    'tokens.access': 'auth'
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        };
+      });
+    })
   });
 };
+
+
 
 UserSchema.pre('save', function (next) {
   var user = this;
